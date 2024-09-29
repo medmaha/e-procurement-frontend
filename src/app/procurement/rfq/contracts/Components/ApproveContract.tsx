aae31916b1e166bb284e1a85216ba44aec6a9832
medmaha <toure925@outlook.com>
2024-09-29 00:37:44 +0000

    Enhanced rfq page

--- src/app/procurement/rfq/contracts/Components/ApproveContract.tsx
+++ src/app/procurement/rfq/contracts/Components/ApproveContract.tsx
@@ -1,7 +1,28 @@
 "use client";
-import ActionConfirmation from "@/Components/ActionConfirmation";
+import React, { useMemo, useRef } from "react";
+import { createContractApproval } from "../actions";
+import { toast } from "react-toastify";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/Components/ui/dialog";
+import { Label } from "@/Components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/Components/ui/select";
+import { Textarea } from "@/Components/ui/textarea";
+import SubmitButton from "@/Components/widget/SubmitButton";
 import { Button } from "@/Components/ui/button";
-import React from "react";
+import Link from "next/link";
 
 type Props = {
   supplier: any;
@@ -9,20 +30,84 @@ type Props = {
 };
 
 export default function ApproveContract({ contract, supplier }: Props) {
+  async function submitApproval(formData: FormData) {
+    try {
+      const response = await createContractApproval(
+        contract.id,
+        formData,
+        location.pathname
+      );
+      if (response.success) {
+        toast.success(response.message);
+        return;
+      }
+      toast.error(response.message);
+    } catch (error) {}
+  }
+
   return (
-    <div className="md:pr-6">
-      <ActionConfirmation
-        onConfirm={async (callback) => {}}
-        confirmText="Yes Approve"
-        description={
-          <>
-            Are you sure you want to approve the contract award issued by{" "}
-            <b>{contract.officer.name}</b> to <b>{supplier.name}</b> ?
-          </>
-        }
-      >
-        <Button size={"sm"}>Approve Contract</Button>
-      </ActionConfirmation>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button size={"sm"}>Approve</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Approve Contract</DialogTitle>
+          <DialogDescription className="text-sm">
+            You can approve the contract award issued by{" "}
+            <Link
+              scroll
+              href={"/users/" + contract.officer.id}
+              className="hover:link hover:underline underline-offset-2 transition"
+            >
+              <strong>{contract.officer.name}</strong>
+            </Link>{" "}
+            to{" "}
+            <Link
+              scroll
+              href={"/suppliers/" + supplier.id}
+              className="hover:link hover:underline underline-offset-2 transition"
+            >
+              <strong>{supplier.name}</strong>
+            </Link>
+            , please confirm.
+          </DialogDescription>
+        </DialogHeader>
+        <form action={submitApproval} className="w-full block space-y-4 mt-4">
+          <div className="grid gap-2">
+            <Label>My Action</Label>
+            <Select name="approve" required>
+              <SelectTrigger>
+                <SelectValue placeholder="Select an option" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="true">Approved</SelectItem>
+                  <SelectItem value="false">Disapproved</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="grid gap-2">
+            <Label>Remarks</Label>
+            <Textarea
+              name="remarks"
+              placeholder="Enter remarks"
+              className="w-full"
+              rows={4}
+              minLength={10}
+              maxLength={500}
+            />
+          </div>
+
+          <div className="mt-4">
+            <SubmitButton
+              text="Submit Approval"
+              className="w-full  md:font-semibold tracking-wide"
+            />
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/app/procurement/rfq/contracts/types.ts
+++ src/app/procurement/rfq/contracts/types.ts
@@ -1,103 +1,119 @@
 interface RFQContract {
-	id: ID;
-	delivery_terms?: string;
-	payment_method?: string;
-	validity_period?: string;
-	pricing?: number;
+  id: ID;
+  delivery_terms?: string;
+  payment_method?: string;
+  validity_period?: string;
+  pricing?: number;
 
-	deadline_date?: string;
-	terms_and_conditions: string;
-	status: "PENDING" | "SUCCESSFUL" | "UNSUCCESSFUL" | "ACTIVE" | "TERMINATED";
+  deadline_date?: string;
+  terms_and_conditions: string;
+  status: "PENDING" | "SUCCESSFUL" | "UNSUCCESSFUL" | "ACTIVE" | "TERMINATED";
 
-	rfq: {
-		id: ID;
-		name: string;
-	};
-	supplier: {
-		id: ID;
-		name: string;
-	};
-	rfq_response: {
-		id: ID;
-		name: string;
-	};
-	officer: {
-		id: ID;
-		name: string;
-	};
-	negotiations: any;
+  rfq: {
+    id: ID;
+    name: string;
+  };
+  supplier: {
+    id: ID;
+    name: string;
+  };
+  rfq_response: {
+    id: ID;
+    name: string;
+  };
+  officer: {
+    id: ID;
+    name: string;
+  };
+  negotiations: any;
 
-	created_date: string;
-	last_modified: string;
+  created_date: string;
+  last_modified: string;
 }
 
 interface RFQNegotiationNote {
-	id: ID;
-	note: string;
-	file?: string;
-	pricing: string;
-	delivery_terms: string;
-	payment_method: string;
-	validity_period: string;
-	accepted: boolean;
-	renegotiated: boolean;
+  id: ID;
+  note: string;
+  file?: string;
+  pricing: string;
+  delivery_terms: string;
+  payment_method: string;
+  validity_period: string;
+  accepted: boolean;
+  renegotiated: boolean;
 
-	contract: {
-		id: ID;
-		name: string;
-	};
-	author: {
-		id: ID;
-		name: string;
-		profile_type: "Vendor" | "Staff";
-	};
-	created_date: string;
-	last_modified: string;
+  contract: {
+    id: ID;
+    name: string;
+  };
+  author: {
+    id: ID;
+    name: string;
+    profile_type: "Vendor" | "Staff";
+  };
+  created_date: string;
+  last_modified: string;
 }
 
 interface RFQContractNegotiation {
-	id: ID;
-	notes: RFQNegotiationNote[];
-	outcome: string;
-	status: "PENDING" | "SUCCESSFUL" | "UNSUCCESSFUL";
-	can_award?: boolean;
-	supplier: {
-		id: ID;
-		name: string;
-	};
-	contract: {
-		id: ID;
-		name: string;
-	};
-	rfq: {
-		id: ID;
-		name: string;
-	};
-	officer: {
-		id: ID;
-		name: string;
-	};
-	created_date: string;
-	last_modified: string;
+  id: ID;
+  notes: RFQNegotiationNote[];
+  outcome: string;
+  status: "PENDING" | "SUCCESSFUL" | "UNSUCCESSFUL";
+  can_award?: boolean;
+  supplier: {
+    id: ID;
+    name: string;
+  };
+  contract: {
+    id: ID;
+    name: string;
+  };
+  rfq: {
+    id: ID;
+    name: string;
+  };
+  officer: {
+    id: ID;
+    name: string;
+  };
+  created_date: string;
+  last_modified: string;
 }
 
 interface RFQContractAward {
-	id: ID;
-	remarks?: string | null;
-	status: "PENDING" | "AWARDED" | "TERMINATED";
-	contract: {
-		id: ID;
-		name: string;
-	};
-	vendor: {
-		id: ID;
-		name: string;
-	};
-	officer: {
-		id: ID;
-		name: string;
-	};
+  id: ID;
+  remarks?: string | null;
+  status: "PENDING" | "AWARDED" | "TERMINATED";
+  contract: {
+    id: ID;
+    name: string;
+  };
+  vendor: {
+    id: ID;
+    name: string;
+  };
+  officer: {
+    id: ID;
+    name: string;
+  };
 
-	created_date: string;
-	last_modified: string;
+  created_date: string;
+  last_modified: string;
+}
+interface ContractApproval {
+  id: ID;
+  remarks?: string | null;
+  approve: ApprovalStatus;
+  contract: {
+    id: ID;
+    name: string;
+    pricing: number;
+  };
+  officer: {
+    id: ID;
+    name: string;
+  };
+  created_date: string;
+  last_modified: string;
 }
