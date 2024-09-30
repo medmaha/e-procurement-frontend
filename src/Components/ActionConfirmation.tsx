733ec5777a9277d703e64440c88c886762f7e4a5
Muhammed Darboe <125799918+DarboeDev@users.noreply.github.com>
2024-09-30 12:06:23 +0000

    Button group and pop-up modal

--- src/Components/ActionConfirmation.tsx
+++ src/Components/ActionConfirmation.tsx
@@ -1,74 +1,82 @@
 "use client";
 import {
-	AlertDialog,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogTitle,
-	AlertDialogTrigger,
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/Components/ui/alert-dialog";
 import { ReactElement, ReactNode, useState } from "react";
 import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
 
 type Props = {
-	disabled?:boolean
-	title?: ReactNode;
-	description?: ReactNode;
-	confirmText?: string;
-	cancelText?: string;
-	children: ReactElement<HTMLButtonElement>;
-	onConfirm: (callback: () => void) => Promise<any>;
-	showLoadingIcon?: boolean;
+  disabled?: boolean;
+  title?: ReactNode;
+  description?: ReactNode;
+  confirmText?: string;
+  cancelText?: string;
+  children: ReactElement<HTMLButtonElement>;
+  onConfirm: (callback: () => void) => Promise<any>;
+  showLoadingIcon?: boolean;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "ghost";
 };
 
 export default function ActionConfirmation(props: Props) {
-	const [isOpen, toggleOpen] = useState(false);
-	const [loading, setLoading] = useState(false);
+  const [isOpen, toggleOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	const { showLoadingIcon = true } = props;
+  const { showLoadingIcon = true } = props;
 
-	async function confirm() {
-		setLoading(true);
-		await props.onConfirm(() => toggleOpen(false));
-		setLoading(false);
-	}
+  async function confirm() {
+    setLoading(true);
+    await props.onConfirm(() => toggleOpen(false));
+    setLoading(false);
+  }
 
-	return (
-		<AlertDialog  open={isOpen} onOpenChange={toggleOpen}>
-			<AlertDialogTrigger disabled={props.disabled} asChild>{props.children}</AlertDialogTrigger>
-			<AlertDialogContent>
-				<AlertDialogHeader>
-					<AlertDialogTitle className="font-semibold">
-						{props.title || "Confirmation"}
-					</AlertDialogTitle>
-					{props.description && (
-						<AlertDialogDescription>{props.description}</AlertDialogDescription>
-					)}
-				</AlertDialogHeader>
-				<AlertDialogFooter>
-					<AlertDialogCancel
-						disabled={loading}
-						className="disabled:pointer-events-none"
-					>
-						{props.cancelText || "No Cancel"}
-					</AlertDialogCancel>
-					<Button disabled={loading} onClick={confirm} className="gap-2">
-						{loading && showLoadingIcon && (
-							<Loader2 className="w-4 h-4 animate-spin" />
-						)}
-						<span
-							className={`dark:font-semibold  ${
-								loading ? "animate-pulse" : ""
-							}`}
-						>
-							{props.confirmText || "Yes Submit"}
-						</span>
-					</Button>
-				</AlertDialogFooter>
-			</AlertDialogContent>
-		</AlertDialog>
-	);
+  return (
+    <AlertDialog open={isOpen} onOpenChange={toggleOpen}>
+      <AlertDialogTrigger disabled={props.disabled} asChild>
+        {props.children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="font-semibold">
+            {props.title || "Confirmation"}
+          </AlertDialogTitle>
+          {props.description && (
+            <AlertDialogDescription>{props.description}</AlertDialogDescription>
+          )}
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel
+            disabled={loading}
+            className="disabled:pointer-events-none"
+          >
+            {props.cancelText || "No Cancel"}
+          </AlertDialogCancel>
+          <Button
+            variant={props.variant}
+            disabled={loading}
+            onClick={confirm}
+            className="gap-2"
+          >
+            {loading && showLoadingIcon && (
+              <Loader2 className="w-4 h-4 animate-spin" />
+            )}
+            <span
+              className={`dark:font-semibold  ${
+                loading ? "animate-pulse" : ""
+              }`}
+            >
+              {props.confirmText || "Yes Submit"}
+            </span>
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
